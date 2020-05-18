6689f13163a8910ca16ffcabcc7ea65a7e0182c7
Vincent <vincentmuijtjens@ziggo.nl>
2020-05-18 12:38:53 +0200

    Added extra routes

--- router/gameguidesService.js
+++ router/gameguidesService.js
@@ -5,7 +5,7 @@ var requestAuthenticator = require('../controllers/requestAuthenticator')
 const apiAdapter = require('./apiAdapter');
 
 const BASE_URL = 'http://localhost:8081';
-const api = apiAdapter(BASE_URL);
+let api = apiAdapter(BASE_URL);
 
 router.get('/gg/supportedgames', requestAuthenticator, (req, res) => {
     api.get(req.path).then(resp => {
@@ -38,6 +38,42 @@ router.get('/gg/lol/guides/:id', requestAuthenticator, (req, res) => {
             res.send("Something went wrong");
             console.log(error)
         })
+});
+
+router.put('/gg/lol/guides/update/:id', requestAuthenticator, (req, res) => {
+    api = apiAdapter(BASE_URL, {"Authorization": req.header("Authorization")});
+    api.put(req.path, req.body).then(resp => {
+        res.send("Your changes are successfully saved.");
+    })
+        .catch(error => {
+            res.send("Something went wrong");
+            console.log(error)
+        });
+    api = apiAdapter(BASE_URL);
+});
+
+router.delete('/gg/lol/guides/delete/:id', requestAuthenticator, (req, res) => {
+    api = apiAdapter(BASE_URL, {"Authorization": req.header("Authorization")});
+    api.delete(req.path).then(resp => {
+        res.send("Your successfully deleted the guide.");
+    })
+        .catch(error => {
+            res.send("Something went wrong");
+            console.log(error)
+        });
+    api = apiAdapter(BASE_URL);
+});
+
+router.post('/gg/lol/guides/add', requestAuthenticator, (req, res) => {
+    api = apiAdapter(BASE_URL, {"Authorization": req.header("Authorization")});
+    api.post(req.path, req.body).then(resp => {
+        res.send("Your guide has been added.");
+    })
+        .catch(error => {
+            res.send("Something went wrong");
+            console.log(error)
+        });
+    api = apiAdapter(BASE_URL);
 });
 
 module.exports = router;
